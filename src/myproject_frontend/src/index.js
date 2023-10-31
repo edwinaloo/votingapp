d32208c1059d96636995cf779e04361d258d884c
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-10-31 17:10:04 +0300

    Initial commit.

---
+++ src/myproject_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { myproject_backend } from "../../declarations/myproject_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await myproject_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
