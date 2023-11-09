2916f4dadf5358f0c4bff685e026abf34d4bb99a
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-11-09 19:35:57 +0300

    basic layout ofourcode push .  changes to javascript

--- src/myproject_frontend/src/script.js
+++ src/myproject_frontend/src/script.js
@@ -1,54 +1,79 @@
-// Import necessary libraries and dependencies for real-time updates
-// and user registration (e.g., Socket.io, Express, Passport, etc.)
-
-// Initialize the WebSocket connection for real-time updates
-const socket = io(); // Replace with your actual WebSocket setup
-
 // Function to display options
 function displayOptions() {
-    // Implement the display of voting options here
-    // You can dynamically create HTML elements to show the options
+    // Simulated data for options
+    const optionsData = [
+        { id: 1, text: "Option 1", votes: 0 },
+        { id: 2, text: "Option 2", votes: 0 },
+        { id: 3, text: "Option 3", votes: 0 },
+    ];
+
+    const optionsContainer = document.getElementById("options");
+
+    optionsData.forEach((option) => {
+        const optionDiv = document.createElement("div");
+        optionDiv.innerHTML = `
+            <label>
+                <input type="radio" name="vote" value="${option.id}">
+                ${option.text}
+            </label>
+        `;
+        optionsContainer.appendChild(optionDiv);
+    });
 }
 
 // Function to handle votes
 function handleVote() {
-    // Implement the voting logic here
-    // You'll need to send the vote to the server and update the UI accordingly
-}
-
-// Simulated login function (for educational purposes)
-function login(username) {
-    // Implement user login and profile creation here
-    // You may use Passport.js or other authentication libraries for this
+    const selectedOption = document.querySelector('input[name="vote"]:checked');
+    if (selectedOption) {
+        const optionId = parseInt(selectedOption.value, 10);
+        // Simulated vote submission (replace with actual logic)
+        console.log(`Voted for option ${optionId}`);
+    } else {
+        console.log("Please select an option before voting.");
+    }
 }
 
 // Function to display user comments and discussions
 function displayComments() {
-    // Implement the display of comments and discussions here
-    // You'll need to fetch and display comments from a server
+    // Simulated data for comments
+    const commentsData = [
+        { username: "User1", comment: "This is great!" },
+        { username: "User2", comment: "I love it!" },
+        { username: "User3", comment: "Not bad." },
+    ];
+
+    const commentsContainer = document.getElementById("comments");
+
+    commentsData.forEach((comment) => {
+        const commentDiv = document.createElement("div");
+        commentDiv.innerHTML = `<strong>${comment.username}:</strong> ${comment.comment}`;
+        commentsContainer.appendChild(commentDiv);
+    });
 }
 
 // Function to display the leaderboard
 function displayLeaderboard() {
-    // Implement the display of the top-voted options in the leaderboard
-    // You'll need to calculate and retrieve the leaderboard data from a server
+    // Simulated data for the leaderboard
+    const leaderboardData = [
+        { rank: 1, option: "Option 1", votes: 10 },
+        { rank: 2, option: "Option 2", votes: 8 },
+        { rank: 3, option: "Option 3", votes: 5 },
+    ];
+
+    const leaderboardContainer = document.getElementById("leaderboard");
+
+    leaderboardData.forEach((entry) => {
+        const entryDiv = document.createElement("div");
+        entryDiv.innerHTML = `<strong>${entry.rank}. ${entry.option}:</strong> ${entry.votes} votes`;
+        leaderboardContainer.appendChild(entryDiv);
+    });
 }
 
-// Event listener for real-time updates (use WebSockets)
-socket.on('voteUpdate', (data) => {
-    // Handle real-time updates for votes and update the UI
-    // This is where you'd update vote counts in real-time
-});
+// Event listener for the Vote button
+const voteButton = document.getElementById("voteButton");
+voteButton.addEventListener("click", handleVote);
 
-// Add event listener for user login
-// Example: Add a login button to the UI and listen for login events
-
-// Add event listener for posting comments and discussions
-// Example: Implement a form for users to post comments and discussions
-
-// Fetch leaderboard data from the server and update the UI
-
-// Initialize the UI with the options, comments, and leaderboard
+// Initialize the UI with options, comments, and leaderboard
 displayOptions();
 displayComments();
 displayLeaderboard();
